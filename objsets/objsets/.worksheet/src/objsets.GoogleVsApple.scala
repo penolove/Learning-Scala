80337b302d4e5bfe57049d05504fabe36251e1f4
Andy <penolove15@gmail.com>
2016-06-20 18:59:23 +0800

    assignment3

---
+++ objsets/objsets/.worksheet/src/objsets.GoogleVsApple.scala
@@ -0,0 +1,15 @@
+package objsets
+
+object GoogleVsApple {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(119); 
+  val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus");System.out.println("""google  : List[String] = """ + $show(google ));$skip(69); 
+  val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad");System.out.println("""apple  : List[String] = """ + $show(apple ));$skip(59); 
+
+  lazy val googleTweets: TweetSet = TweetReader.allTweets;System.out.println("""googleTweets: => objsets.TweetSet""")}
+  //lazy val appleTweets: TweetSet = TweetReader.allTweets.filter(tw => apple.exists(e => tw.text.contains(e)))
+  
+  /**
+   * A list of all tweets mentioning a keyword from either apple or google,
+   * sorted by the number of retweets.
+   */
+    // lazy val trending: TweetList = (googleTweets.union(appleTweets)).descendingByRetweet
+  }
---
+++ objsets/objsets/.worksheet/src/objsets.www.scala
@@ -0,0 +1,5 @@
+package objsets
+
+object www {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(73); 
+  println("Welcome to the Scala worksheet")}
+}
